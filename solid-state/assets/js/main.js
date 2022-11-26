238459e3b4caaaf5f0f1135a70c61d5770ac4cd5
Euclid <hearingcolorsatx@gmail.com>
2022-11-25 22:30:36 -0600

    updated initial layout

--- solid-state/assets/js/main.js
+++ solid-state/assets/js/main.js
@@ -13,11 +13,11 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
+			// xlarge:	'(max-width: 1680px)',
+			// large:	'(max-width: 1280px)',
+			// medium:	'(max-width: 980px)',
+			// small:	'(max-width: 736px)',
+			// xsmall:	'(max-width: 480px)'
 		});
 
 	// Play initial animations on page load.
